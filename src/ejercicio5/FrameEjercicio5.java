5c2236a95c1bfec88732e93754f584696fb3b98e
Hugo Mendez <hugo3011mendez@gmail.com>
2020-06-04 11:21:25 +0200

    Añadida funcionalidad de CheckBox y activar el botón Jugar

--- src/ejercicio5/FrameEjercicio5.java
+++ src/ejercicio5/FrameEjercicio5.java
@@ -27,7 +27,8 @@ public class FrameEjercicio5 extends JFrame implements ActionListener, ItemListe
 
     int[] numeros = new int[49];
 
-    int numsEscogidos; // Creo una variable para controlar que se han escogido 6 números
+    int numsEscogidos = 0; // Creo una variable para controlar que se han escogido 6 números
+    ArrayList<JCheckBox> checkNumeros = new ArrayList<>(); // Creo una colección donde guardar los checkbox de los números
 
     public FrameEjercicio5(){
         super("Ejercicio 5 Boletín Tema 9");
@@ -35,12 +36,38 @@ public class FrameEjercicio5 extends JFrame implements ActionListener, ItemListe
 
         llenarArray(numeros);
 
+        for (int i = 0; i < numeros.length; i++) {
+            JCheckBox chkNumero = new JCheckBox(String.valueOf(numeros[i])); // Creo un checkbox por cada número
+            chkNumero.setSize(chkNumero.getPreferredSize());
+            chkNumero.addActionListener(this);
+            this.add(chkNumero);
+            checkNumeros.add(chkNumero);
+        }
+
+        btnJugar = new JButton("Jugar");
+        btnJugar.setSize(btnJugar.getPreferredSize());
+        btnJugar.setEnabled(false);
+        btnJugar.addActionListener(this);
+        add(btnJugar);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        if(e.getSource().getClass() == JCheckBox.class){ // Acciones a realizar si se ha interactuado con un JCheckBox
 
+            for (int i = 0; i < checkNumeros.size(); i++) {
+                if(e.getSource() == checkNumeros.get(i) && checkNumeros.get(i).isSelected()){
+                    numsEscogidos ++;
+                }
+                else if(e.getSource() == checkNumeros.get(i) && !checkNumeros.get(i).isSelected()){
+                    numsEscogidos --;
+                }
+            }
+
+            if(numsEscogidos == 6){
+                btnJugar.setEnabled(true);
+            }
+        }
     }
 
     @Override
